f13fe85f6b10470c1901934b78c6037797d6643d
LongND <nguyenduclong12a2nd@gmail.com>
2023-09-27 14:13:43 +0700

    add example with button run thread

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <wx/wx.h>
+#include <thread>
 
 namespace FrameExample {
   class Frame : public wxFrame {
@@ -16,6 +17,15 @@ namespace FrameExample {
         wxTheApp->ExitMainLoop();
       });
 
+      buttonThread->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event){
+        std::thread t1([]{Frame::threadFunction1();});
+        std::thread t2([]{Frame::threadFunction2();});
+
+        // Wait for both threads to finish
+        t1.join();
+        t2.join();
+      });
+
       Bind(wxEVT_CLOSE_WINDOW, [&](wxCloseEvent& event) {
         if (wxMessageBox("Are you sure you want exit?", "Close Window", wxICON_QUESTION|wxYES_NO) != wxYES) event.Veto();
         else event.Skip();
@@ -27,6 +37,20 @@ namespace FrameExample {
     wxButton* buttonClose = new wxButton(panel, wxID_ANY, "Close", {10, 10});
     wxButton* buttonExit = new wxButton(panel, wxID_ANY, "Exit", {100, 10});
     wxButton* buttonExitMainLoop = new wxButton(panel, wxID_ANY, "ExitMainLoop", {190, 10});
+    wxButton* buttonThread = new wxButton(panel, wxID_ANY, "Run Thread", wxPoint(300, 10));
+    static void threadFunction1() {
+        // Code to be executed in thread 1
+        for (int i = 0; i < 5; ++i) {
+            std::cout << "Thread 1: " << i << std::endl;
+        }
+    }
+
+    static void threadFunction2() {
+        // Code to be executed in thread 2
+        for (int i = 0; i < 5; ++i) {
+            std::cout << "Thread 2: " << i << std::endl;
+        }
+    }
   };
 
   class Application : public wxApp {
